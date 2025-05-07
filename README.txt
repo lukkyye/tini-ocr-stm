9c401fde7ede1c86351071b935b41984bf4acb16
lukkyye <lucas.icasetti@fing.edu.uy>
2025-05-07 15:48:40 -0300

    refactor(all): change framework from tesseract to easyocr

--- README.txt
+++ README.txt
@@ -1,4 +1,3 @@
-1. Instalar Tesseract
-2. Exportar al bashrc o en terminal mismo, la direccion del directirio de tessdata como export TESSDATA_PREFIX=<dir> yo la tengo en /usr/share/tessdata
-3. venv + requirements
+1. venv + requirements
+2. Ejecutar main.py
 
--- src/main.py
+++ src/main.py
@@ -1,12 +1,7 @@
+import easyocr as ocr
 import cv2
-import PIL
-import pytesseract
+import time
 
-def is_valid(string_name: str)->bool:
-    return (string_name.replace(' ', '').isalnum())
-
-#robadita de stackoverflow
-###########################################################
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
@@ -21,61 +16,40 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
         dim = (width, int(h * r))
 
     return cv2.resize(image, dim, interpolation=inter)
-###########################################################
 
+
+reader = ocr.Reader(["es"], gpu=False)
 capture = cv2.VideoCapture(0)
 
-def parse(info: str, code: str)->dict:
-    if code=='':
-        return None
-    
-    
-    pre = {}
-    if "Cl " in info:
-        found="Cl "
-    elif "CI " in info:
-        found="CI "
-    else:
-        return None
-    
-    try:
-        pre["cardcode"]=''.join([i for i in code if i.isalnum()])
-        idx = info.index(found)
-        pre["id"]=info[idx+2:idx+14].strip().replace(' ', '').replace("-", '').replace(".", '')
-        pre["id"].strip()
-        pre["name"]=info.split("ESTUDIANTE")[1].split(found)[0].splitlines()
-        pre["name"]=[i for i in pre["name"] if i.isupper()]
-        for i in pre["name"]:
-            if not is_valid(i):
-                return None
-    
-        pre["name"]=' '.join(pre["name"])
-        print(pre)
-        return pre
-    except:
-        return None
+#triggerear con rfid
+rfid=True
 
+cards = []
+while capture.isOpened():
+    if rfid:
+        ret, frame = capture.read()
+        frame = ResizeWithAspectRatio(frame, 640)
+        result = reader.readtext(frame)[0:4]
+        
+        person = {}
+        prelist = [j for i, j, k in result if k>=0.45]
+        try:
+            person["name"]=prelist[1]
+            person["surname"]=prelist[2]
+            person["id"]=prelist[3][3:]
 
-def get_code(image)->str:
-    rotated90deg = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    rotated90deg = rotated90deg[0:250, 200:720] #<-----AJUSTAR SEGUN RESOLUCION DE CAMARA
-    return pytesseract.image_to_string(PIL.Image.fromarray(rotated90deg), lang='spa')[0:8]
+            #cardcode
+            rotated90deg = cv2.rotate(frame[100:400, 400:640], cv2.ROTATE_90_COUNTERCLOCKWISE)
+            result_cardcode= reader.readtext(rotated90deg)[0]
+            person["card_code"]=result_cardcode[1]
+            
+            
+            print(person)
+            cards.append(person)
+            time.sleep(1)
+        except:
+            print("Retrying...")
+            continue
 
-while (capture.isOpened()):
-    ret, frame = capture.read()
-    
-    imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    ret, thresh = cv2.threshold(imgray, 120, 200, 0)
-    
-    cv2.imshow("webcam", thresh)
-    
-    info = pytesseract.image_to_string(
-        PIL.Image.fromarray(cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB)),
-        lang='spa')
-    cardcode = get_code(thresh)
-    
-    print(parse(info, cardcode))
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+capture.release()
+cv2.destroyAllWindows()
